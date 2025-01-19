12d6efc2f2c5482bb624e478874bd5e328d5cf59
Manas <iamscientistmanas@gmail.com>
2025-01-19 17:34:32 +0530

    Add landing page: Fix error

--- Frontend/app/(default_site)/auth/page.tsx
+++ Frontend/app/(default_site)/auth/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { useState } from "react";
+// import { useState } from "react";
 import { PageLoader } from "@/app/ui/loaders";
 
 export default function Auth() {
-  const [mounted, setMounted] = useState(false);
-  setMounted(false);
+  // const [mounted, setMounted] = useState(false);
   // const [authSuccess, setAuthSuccess] = useState(false);
 
-  if (!mounted) return <PageLoader />;
-  else return <div className="auth"></div>;
+  // if (!mounted) return <PageLoader />;
+  // else
+  // return <div className="auth"></div>;
+  return <PageLoader />;
 }
